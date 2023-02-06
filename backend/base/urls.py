1c9ed709cd914a32bb3862e890c4856d7ae3d82d
rya <reeyashrestha00@gmail.com>
2023-02-06 21:10:25 +0545

    before django signals

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-)
+
 
 # Configuring urls to know when to trigger the views
 urlpatterns = [
-    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('', views.getRoutes, name="routes"),
+    path('users/login/', views.MyTokenObtainPairView.as_view(), 
+            name='token_obtain_pair'),
+    path('users/register/', views.registerUser, name='register'),
+    path('users/profile/', views.getUserProfile, name="user-profile"),
+    path('users/', views.getUsers, name="users"),
     path('products/', views.getProducts, name="products"),
     path('products/<str:pk>/', views.getProduct, name="product"),
 ]
