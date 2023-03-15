4cf11a7301be930be7796fee65a6c41ed726b540
Bidhan Bajracharya <95129959+Bidhan-Bajracharya@users.noreply.github.com>
2023-03-15 21:04:13 +0545

    Added bit of hover effect and aligned texts on painting cards

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -10,27 +10,29 @@ function Product({ product }) {
   return (
     <Link to={`/product/${product._id}`}>
       <Card className="product-card">
-        
-          <Card.Img src={`${SEED}${product.image}`} />
-        
+        <Card.Img src={`${SEED}${product.image}`} />
 
-        <Card.Body>
-            <Card.Title as="div">
-              <strong className="painting-title">{product.name}</strong>
-            </Card.Title>
-          
+        <Card.Body className="product-card-body">
+          <div className="product-card-title">
+            <label className="product-card-label">Title: </label>
+            <p className="painting-title">{product.name}</p>
+          </div>
 
           <Card.Text as="div">
-            <div className="my-3">
+            <div className="product-review-container">
+              <label className="product-card-label">Reviews: </label>
               <Rating
                 value={product.rating}
-                text={`${product.numReviews} reviews`}
+                text={`${product.numReviews}`}
                 color={"#f8e826"}
               />
             </div>
           </Card.Text>
 
-          <Card.Text as="h3">Rs. {product.price}</Card.Text>
+          <div className="product-price-container">
+            <label className="product-card-label">Price: </label>
+            <p className="price">Rs. {product.price}</p>
+          </div>
         </Card.Body>
       </Card>
     </Link>
