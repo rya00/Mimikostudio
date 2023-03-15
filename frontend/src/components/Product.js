c7b06fa22b30cd5f64f007b007915c1a3cf1f941
Bidhan Bajracharya <95129959+Bidhan-Bajracharya@users.noreply.github.com>
2023-03-15 20:01:44 +0545

    Made the whole product card as a route

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,37 +1,40 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
-import { SEED } from '../env'
-import Rating from './Rating'
-import { Link } from 'react-router-dom'
-import "../index.css"
+import React from "react";
+import { Card } from "react-bootstrap";
+import { SEED } from "../env";
+import Rating from "./Rating";
+import { Link } from "react-router-dom";
+import "../index.css";
 
 // Product({ product }) -> Accessing products directly without using props
 function Product({ product }) {
   return (
-    <Card className='my-3 p-3 rounded product-card'>
-        <Link to={`/product/${product._id}`}> 
-            <Card.Img src={`${SEED}${product.image}`} />
-        </Link>
+    <Link to={`/product/${product._id}`}>
+      <Card className="product-card">
+        
+          <Card.Img src={`${SEED}${product.image}`} />
+        
 
         <Card.Body>
-            <Link to={`/product/${product._id}`}> 
-                <Card.Title as="div">
-                    <strong>{product.name}</strong>
-                </Card.Title>
-            </Link>
+            <Card.Title as="div">
+              <strong className="painting-title">{product.name}</strong>
+            </Card.Title>
+          
 
-            <Card.Text as='div'>
-                <div className='my-3'>
-                    <Rating value = {product.rating} text={`${product.numReviews} reviews`} color={'#f8e826'} />
-                </div>
-            </Card.Text>
+          <Card.Text as="div">
+            <div className="my-3">
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+                color={"#f8e826"}
+              />
+            </div>
+          </Card.Text>
 
-            <Card.Text as='h3'>
-                Rs. {product.price}
-            </Card.Text>
-        </Card.Body>        
-    </Card>
-  )
+          <Card.Text as="h3">Rs. {product.price}</Card.Text>
+        </Card.Body>
+      </Card>
+    </Link>
+  );
 }
 
-export default Product
+export default Product;
