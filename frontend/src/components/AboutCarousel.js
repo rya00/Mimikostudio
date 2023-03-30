b3e0a4a94293bfa2c08f61deee34bea07af08d32
Bidhan Bajracharya <95129959+Bidhan-Bajracharya@users.noreply.github.com>
2023-03-30 17:47:05 +0545

    Separated homeScreen layout from other page's layout + tried things on carousel

--- frontend/src/components/AboutCarousel.js
+++ frontend/src/components/AboutCarousel.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import { Carousel } from 'react-bootstrap'
+import React from "react";
+import { Carousel, Image } from "react-bootstrap";
 
 const AboutCarousel = () => {
   return (
     <>
-        <Carousel pause='hover' className='about-carousel'>
-        <Carousel.Item>brodor</Carousel.Item>
-        <Carousel.Item>sister</Carousel.Item>
-        </Carousel>
+      <Carousel pause="hover" className="about-carousel">
+        <Carousel.Item>
+          {/* <Image src="https://picsum.photos/200/300" alt="img" fluid /> */}
+          <div className="carousel-img-container">
+        ss
+          {/* <img src="https://picsum.photos/200/300" alt="painting" ></img> */}
+          </div>
+        </Carousel.Item>
+      </Carousel>
     </>
-  )
-}
+  );
+};
 
-export default AboutCarousel
+export default AboutCarousel;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,36 +1,37 @@
 // useState and useEffect used for connecting to backend
-import React, {useState, useEffect} from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col } from 'react-bootstrap'
-import { useNavigate, useLocation } from 'react-router-dom'
-import Product from '../components/Product'
-import Loader from '../components/Loader'
-import Message from '../components/Message'
-import Paginate from '../components/Paginate'
-import ProductCarousel from '../components/ProductCarousel'
-import { listProducts } from '../actions/productActions'
-import AboutCarousel from '../components/AboutCarousel'
-import useUser from '../helper/useUser'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col } from "react-bootstrap";
+import { useNavigate, useLocation } from "react-router-dom";
+import Product from "../components/Product";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import Paginate from "../components/Paginate";
+import ProductCarousel from "../components/ProductCarousel";
+import { listProducts } from "../actions/productActions";
+import AboutCarousel from "../components/AboutCarousel";
+import useUser from "../helper/useUser";
+import { Container } from "react-bootstrap";
 
 /* <Message>{error}</Message> -> Error is passed as child in the component */
 
 function HomeScreen() {
-  const navigate = useNavigate()
-  const location = useLocation()
-  
-  const dispatch = useDispatch()
-  const productList = useSelector(state => state.productList)
-  // Destructuring productListReducer
-  const {error, loading, products, page, pages} = productList
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  let keyword = location.search
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  // Destructuring productListReducer
+  const { error, loading, products, page, pages } = productList;
+
+  let keyword = location.search;
 
   // useEffect ->  Triggered every single time the component loads or when a state value gets updated
   // In our app it gets triggered when the component first loads
   // Empty array kept because we only want this to update when the component loads not when an actual state element gets updated
   useEffect(() => {
-    dispatch(listProducts(keyword))
-  }, [dispatch, keyword]) 
+    dispatch(listProducts(keyword));
+  }, [dispatch, keyword]);
 
   console.log(products);
 
@@ -38,24 +39,29 @@ function HomeScreen() {
     <div>
       {/* {!keyword && <ProductCarousel />} */}
       <AboutCarousel />
-      <h1 className='auto-underline-animation'>Latest Products</h1>
-      {loading ? <Loader />
-        : error ? <Message variant='danger'>{error}</Message>
-          :
+
+      <Container>
+        <h1 className="auto-underline-animation">Latest Products</h1>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
           <div>
-            <Row className='painting-container'>
-              {products.map(product => (
-                  // Key attribute is needed to map for looping in react
-                  <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                      <Product product ={product} />
-                  </Col>
+            <Row className="painting-container">
+              {products.map((product) => (
+                // Key attribute is needed to map for looping in react
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                  <Product product={product} />
+                </Col>
               ))}
             </Row>
-            <Paginate page={page} pages={pages} keyword={keyword}/>
+            <Paginate page={page} pages={pages} keyword={keyword} />
           </div>
-      }      
+        )}
+      </Container>
     </div>
-  )
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
